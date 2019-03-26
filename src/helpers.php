261de3db8b3a721ac302532e21f25db966650022
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-03-26 15:17:30 +0200

    Return array not a collection in nova_get_locales

--- src/helpers.php
+++ src/helpers.php
@@ -5,7 +5,6 @@ use OptimistDigital\NovaLocaleManager\Models\Locale;
 if (!function_exists('nova_get_locales')) {
     function nova_get_locales()
     {
-        return Locale::all()->pluck('name', 'locale');
+        return Locale::all()->pluck('name', 'locale')->toArray();
     }
 }
-
