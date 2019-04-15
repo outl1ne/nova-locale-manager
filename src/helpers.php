e30f2c38a884301a4787818ac22efc9fb48a7a54
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-04-15 14:44:39 +0300

    Sort by default

--- src/helpers.php
+++ src/helpers.php
@@ -6,7 +6,11 @@ if (!function_exists('nova_get_locales')) {
     function nova_get_locales(): array
     {
         try {
-            return Locale::all()->pluck('name', 'locale')->toArray();
+            return Locale
+                ::orderBy('default', 'desc')
+                ->get()
+                ->pluck('name', 'locale')
+                ->toArray();
         } catch (\Exception $e) {
             return [];
         }
@@ -17,13 +21,17 @@ if (!function_exists('nova_get_locales_full')) {
     function nova_get_locales_full(): array
     {
         try {
-            return Locale::all()->map(function ($locale) {
-                return [
-                    'name' => $locale->name,
-                    'slug' => $locale->locale,
-                    'default' => $locale->default,
-                ];
-            })->toArray();
+            return Locale
+                ::orderBy('default', 'desc')
+                ->get()
+                ->map(function ($locale) {
+                    return [
+                        'name' => $locale->name,
+                        'slug' => $locale->locale,
+                        'default' => $locale->default,
+                    ];
+                })
+                ->toArray();
         } catch (\Exception $e) {
             return [];
         }
