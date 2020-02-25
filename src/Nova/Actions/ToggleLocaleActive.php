b286d1c7773b043ab5033acbd7484b5c4c85821e
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-02-25 16:06:18 +0200

    Add ToggleLocaleActive action, improve index view UI

---
+++ src/Nova/Actions/ToggleLocaleActive.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OptimistDigital\NovaLocaleManager\Nova\Actions;
+
+use Laravel\Nova\Actions\Action;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Fields\ActionFields;
+
+class ToggleLocaleActive extends Action
+{
+    public $name = 'Toggle active';
+    public $showOnTableRow = true;
+
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        foreach ($models as $locale) {
+            $locale->active = !$locale->active;
+            $locale->save();
+        }
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return [];
+    }
+}
--- src/Nova/Resources/Locale.php
+++ src/Nova/Resources/Locale.php
@@ -8,22 +8,34 @@ use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Boolean;
 use OptimistDigital\NovaLocaleManager\Models\Locale as LocaleModel;
 use OptimistDigital\NovaLocaleManager\Nova\Actions\MakeLocaleDefault;
+use OptimistDigital\NovaLocaleManager\Nova\Actions\ToggleLocaleActive;
 
 class Locale extends Resource
 {
     public static $model = LocaleModel::class;
     public static $title = 'name';
-    public static $search = ['name', 'locale'];
+    public static $search = ['name', 'slug'];
     public static $displayInNavigation = false;
 
     public function fields(Request $request)
     {
         return [
-            Text::make('Name', 'name'),
+            Text::make('Locale', function ($model) {
+                $defaultSvg = $model->default ? "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" aria-labelledby=\"check-circle\" role=\"presentation\" class=\"fill-current ml-2 text-success\"><path d=\"M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z\"></path></svg>" : '';
+                return "<div class='flex align-center'><span style='line-height: 22px;'><b>{$model->name}</b> ({$model->slug})</span>${defaultSvg}</div>";
+            })
+                ->asHtml()
+                ->onlyOnIndex(),
+
+            Text::make('Name', 'name')->hideFromIndex(),
+
             Text::make('Slug', 'slug')
-                ->help('ISO-639'),
+                ->help('ISO-639')
+                ->hideFromIndex(),
+
             Boolean::make('Default', 'default')
-                ->exceptOnForms(),
+                ->hideFromIndex(),
+
             Boolean::make('Active', 'active'),
         ];
     }
@@ -32,6 +44,7 @@ class Locale extends Resource
     {
         return [
             new MakeLocaleDefault,
+            new ToggleLocaleActive,
         ];
     }
 }
